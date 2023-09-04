c8d39fee2b1bebd1b9676d81570ec80d3905be88
Damián Ariel López Peduzzi <116690423+pridegold@users.noreply.github.com>
2023-09-04 17:53:35 -0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,114 @@
+-- Ingreso Bruto
+
+SELECT cast(SUM(total_price) as int) AS Total_Revenue FROM Pizza_Sales$;
+
+-- Ticket Promedio
+
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales
+
+-- Pizzas Vendidas
+
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales
+
+-- Total Ordenes
+
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales
+
+
+-- Promedio de pizzas por ordenes -- Doble Cast para poder tener un numero decimal optimo x,xx
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales
+
+-- Ordenes Por Dia
+-- Datename(dw, order_date) forma de poder sacar el dia.
+
+SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
+FROM pizza_sales
+GROUP BY DATENAME(DW, order_date)
+
+-- Ordenes Por Mes
+
+select DATENAME(MONTH, order_date) as Month_Name, COUNT(DISTINCT order_id) as Total_Orders
+from pizza_sales
+GROUP BY DATENAME(MONTH, order_date)
+
+-- % de ventas segun tipo de pizza anual
+
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+-- si quisieramos buscar por mes
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales where month(order_date) = 1
+GROUP BY pizza_category
+
+-- % de ventas segun tamaño de pizza
+
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_size
+ORDER BY pizza_size
+
+-- Unidades de pizzas vendidas por categoria anual
+
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza_sales
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC
+
+-- Si quisieramos filtrar por mes
+
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza_sales WHERE MONTH(order_date) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC
+
+-- Top 5 Pizzas
+
+SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue DESC
+
+-- Under 5 Pizzas
+
+SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue ASC
+
+
+-- Top 5 pizzas vendidas por año
+
+SELECT Top 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+
+-- Under top 5
+SELECT Top 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold asc
+
+-- Ahora hacemos lo mismo, pero en vez de calcular la cantidad de pizzas, vamos a calcular en que cantidad de ordenes aparecen
+
+SELECT Top 5 pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders DESC
+
+-- Bottom 5
+SELECT Top 5 pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders asc
+
+
+
